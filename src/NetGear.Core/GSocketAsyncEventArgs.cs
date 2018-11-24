881563481d9a61804ed13173929c58fa806f0b4b
pinopino <myhinata@126.com>
2018-11-24 16:55:55 +0800

    streamedconection的eap改造0.8.5

---
+++ src/NetGear.Core/GSocketAsyncEventArgs.cs
@@ -0,0 +1,41 @@
+﻿using NetGear.Core.Threading;
+using System;
+using System.Net.Sockets;
+
+namespace NetGear.Core
+{
+    public sealed class GSocketAsyncEventArgs<TToken> : SocketAsyncEventArgs
+    {
+        IScheduler _scheduler;
+
+        public TToken Token
+        {
+            set { UserToken = value; }
+            get { return (TToken)UserToken; }
+        }
+
+        public GSocketAsyncEventArgs()
+        {
+        }
+
+        public GSocketAsyncEventArgs(IScheduler scheduler)
+        {
+            if (scheduler == null)
+                throw new ArgumentNullException("scheduler");
+
+            _scheduler = scheduler;
+        }
+
+        protected override void OnCompleted(SocketAsyncEventArgs e)
+        {
+            if (_scheduler != null)
+            {
+                _scheduler.QueueTask(p => base.OnCompleted((SocketAsyncEventArgs)p), e);
+            }
+            else
+            {
+                base.OnCompleted(e);
+            }
+        }
+    }
+}
